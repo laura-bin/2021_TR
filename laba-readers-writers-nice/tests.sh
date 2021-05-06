197b3e53055845d9e4a65e2e104086b4e0e00457
lbin <l.binacchi@gmail.com>
2021-05-06 08:23:39 +0000

    new tests values

--- laba-readers-writers-nice/tests.sh
+++ laba-readers-writers-nice/tests.sh
@@ -1,7 +1,7 @@
 for i in {0..99}
 do
-    sudo ./build/reader_writer 50 19 10 -20 10
-    sudo ./build/reader_writer 50 -20 10 19 10
-    sudo ./build/reader_writer 50 0 10 19 10
-    sudo ./build/reader_writer 50 0 10 0 10
+    ./build/reader_writer 50 19 10 -20 10
+    ./build/reader_writer 50 -20 10 19 10
+    ./build/reader_writer 50 0 10 19 10
+    ./build/reader_writer 50 0 10 0 10
 done > /dev/null
