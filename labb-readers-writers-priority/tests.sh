223c0a9daa2fdebc151076f5b8db28bb98d74e3b
lbin <l.binacchi@gmail.com>
2021-05-09 15:09:17 +0000

    update tests

--- labb-readers-writers-priority/tests.sh
+++ labb-readers-writers-priority/tests.sh
@@ -1,7 +1,6 @@
-for i in {0..99}
+for i in {1..50}
 do
-    ./build/reader_writer 50 19 10 -20 10
-    ./build/reader_writer 50 -20 10 19 10
-    ./build/reader_writer 50 0 10 19 10
-    ./build/reader_writer 50 0 10 0 10
+    sudo ./build/reader_writer 60 10 5 20 20
+    sudo ./build/reader_writer 60 20 5 10 20
+    sudo ./build/reader_writer 60 10 5 10 20
 done > /dev/null
