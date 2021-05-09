f1bef345f11fb40743d660926b5ab9006e840ad5
lbin <l.binacchi@gmail.com>
2021-05-09 17:07:07 +0000

    update tests

--- laba-readers-writers-nice/tests.sh
+++ laba-readers-writers-nice/tests.sh
@@ -1,6 +1,7 @@
-for i in {0..99}
+for i in {1..50}
 do
-    ./build/reader_writer 60 19 5 -20 20
-    ./build/reader_writer 60 -20 5 19 20
-    ./build/reader_writer 60 0 5 0 20
+    ./build/reader_writer 60 1 5 1 20
+    # ./build/reader_writer 60 19 5 -20 20
+    # ./build/reader_writer 60 -20 5 19 20
+    # ./build/reader_writer 60 0 5 0 20
 done > /dev/null
